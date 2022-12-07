dbbbe659659b95e2c24120d815ef3c0235786b91
lachfoy <lachie.foy7@gmail.com>
2022-12-07 11:18:53 +1100

    add map

--- main.js
+++ main.js
@@ -4,13 +4,48 @@ const ctx = canvas.getContext("2d");
 import { Input } from "/input.js";
 var input = new Input(canvas);
 
+/// MAP stuff
+var mapWidth = 10;
+var mapHeight = 10;
+
+// 10x10 map data as 1d string
+var mapData = "1111111111";
+   mapData += "1000000101";
+   mapData += "1011110101";
+   mapData += "1010000001";
+   mapData += "1010111011";
+   mapData += "1000100001";
+   mapData += "1010001101";
+   mapData += "1010111001";
+   mapData += "1010000011";
+   mapData += "1111111111";
+
+// player stuff
+var playerX = 1;
+var playerY = 1;
+
 var deltaTimeStr;
 function update(deltaTime) {
   deltaTimeStr = deltaTime.toPrecision(5);
 }
 
+ctx.imageSmoothingEnabled = false;
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // draw map
+  const tileSize = 8;
+  for (var x = 0; x < mapWidth; x++) {
+    for (var y = 0; y < mapHeight; y++) {
+      if (mapData[x + mapWidth * y] == 1) {
+        ctx.strokeRect(x * tileSize, y * tileSize, tileSize, tileSize);
+      }
+    }
+  }
+
+  // draw player
+  ctx.fillStyle = "blue";
+  ctx.fillRect(playerX * tileSize, playerY * tileSize, tileSize, tileSize);
 
   // draw delta time
   ctx.fillStyle = "black";
